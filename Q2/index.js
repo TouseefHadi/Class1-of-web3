69ffd0d0d19991ae278d02107a9a26291757dc57
TouseefHadi <121400946+TouseefHadi@users.noreply.github.com>
2023-07-09 17:47:45 +0500

    Add files via upload

---
+++ Q2/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var names = "Touseef hadi";
+//now printing upper case 
+var uppercase_name = names.toUpperCase();
+console.log("Name in upper case is: ", uppercase_name);
+//now printing lower case 
+var lowerCase_name = names.toLowerCase();
+console.log("Name in lower case is: ", lowerCase_name);
+//now printing title case 
+var titlcase_name = names.toLowerCase().replace(/(^|\s)\S/g, (char) => char.toUpperCase());
+console.log("Name in title case is: ", titlcase_name);
